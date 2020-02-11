feac6b72b7a7a23242e3cc43b3f087e7cf73409b
s-prianto <prianto2796@gmail.com>
2020-02-11 07:14:07 +0700

    Tugas 2 JS

---
+++ tugas2.js
@@ -0,0 +1,15 @@
+var jumlah_buah = 1;
+var nama_buah = "jeruk";
+var buah_jeruk = true;
+var buah_mangga = null;
+var buah_durian;
+var nama_nama_buah = ["jeruk","manggis","pisang","pepaya"];
+
+
+console.log(jumlah_buah);
+console.log(nama_buah);
+console.log(buah_jeruk);
+console.log(buah_mangga);
+console.log(buah_durian);
+console.log(nama_nama_buah);
+console.log(nama_nama_buah[2]);
